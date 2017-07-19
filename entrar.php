c96cda3be57a6144728664a58985acd736493763
JuniimSI <jrfernandesti@gmail.com>
2017-07-19 12:48:36 -0300

    deletando já na pasta

--- entrar.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$conexao = mysql_connect('localhost','root','');
-	mysql_select_db('android', $conexao);
-	$sql = "select * from integrante";
-	$resultado = mysql_query($sql) or die ("Erro:". mysql_error());
-
-?>
